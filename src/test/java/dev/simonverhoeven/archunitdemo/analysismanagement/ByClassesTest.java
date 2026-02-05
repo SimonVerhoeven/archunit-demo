405dc7c59319d2d525b3e166e53cce4ab4b809d5
Simon Verhoeven <verhoeven.simon@gmail.com>
2026-02-05 19:23:34 +0100

    feature: leverage new old date and time rule

--- src/test/java/dev/simonverhoeven/archunitdemo/analysismanagement/ByClassesTest.java
+++ src/test/java/dev/simonverhoeven/archunitdemo/analysismanagement/ByClassesTest.java
@@ -6,6 +6,7 @@ import com.tngtech.archunit.lang.ArchRule;
 import dev.simonverhoeven.archunitdemo.legacymodule.LegacyService;
 
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+import static com.tngtech.archunit.library.GeneralCodingRules.OLD_DATE_AND_TIME_CLASSES_SHOULD_NOT_BE_USED;
 
 // When a class might shift location, and we want to determine its package location
 @AnalyzeClasses(packagesOf = LegacyService.class)
@@ -13,4 +14,5 @@ public class ByClassesTest {
 
     @ArchTest
     private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION.because("error from package level analysis");
+    private final ArchRule no_old_date_and_time_usage = OLD_DATE_AND_TIME_CLASSES_SHOULD_NOT_BE_USED.because("these go against our coding conventions");
 }
